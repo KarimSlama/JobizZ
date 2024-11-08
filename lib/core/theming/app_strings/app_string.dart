a290d22410a6679f7f4be4a561ba82ea3c8169de
karim_slama <karimslama917@gmail.com>
2024-11-08 20:22:02 +0200

    - create and design splash_screen - make the system style - add some strings for the app and used colors as well as create two theme file for light and dark - modify pubspec.yaml and add some packages - modify main file

---
+++ lib/core/theming/app_strings/app_string.dart
@@ -0,0 +1,65 @@
+class AppString {
+  static const searchYourJob = 'Search your job';
+  static const figureOutYourTopFivePrioritiesWhetherItIsCompanyCultureSalary =
+      'Figure out your top five priorities whether it is company culture, salary.';
+  static const browseJobsList = 'Browse jobs list';
+  static const ourJobListIncludeSeveralIndustriesSoYouCanFindTheBestJobForYou =
+      'Our job list include several  industries, so you can find the best job for you.';
+  static const applyToBestJobs = 'Apply to best jobs';
+  static const youCanApplyToYourDesirableJobsVeryQuicklyAndEasilyWithEase =
+      'You can apply to your desirable jobs very quickly and easily with ease.';
+  static const makeYourCareer = 'Make your career';
+  static const weHelpYouFindYourDreamJobBasedOnYourSkillsetLocationDemand =
+      'We help you find your dream job based on your skillset, location, demand.';
+  static const searchYourDreamJobFastAndEase =
+      'Search your dream job fast and ease';
+  static const figureOutYourTopFivePrioritiesWhetherItIsCompanyCultureSalaryOrASpecificJobPosition =
+      'Figure out your top five priorities whether it is company culture, salary or a specific job position.';
+  static const makeYourDreamCareerWithJob = 'Make your dream career with job';
+  static const weHelpYouFindYourDreamJobAccordingToYourSkillsetLocationPreferenceToBuildYourCareer =
+      'We help you find your dream job according to your skillset, location & preference to build your career.';
+  static const skip = 'Skip';
+  static const next = 'Next';
+  static const jobizz = 'Jôbizz';
+  static const registration = 'Registration👍';
+  static const letsRegisterApplyToJobs = 'Let’s Register. Apply to jobs!';
+  static const fullName = 'Full Name';
+  static const email = 'E-mail';
+  static const phoneNumber = 'Phone Number';
+  static const password = 'Password';
+  static const newPassword = 'New Password';
+  static const confirmPassword = 'Confirm Password';
+  static const register = 'Register';
+  static const orContinueWith = 'Or continue with';
+  static const haveAnAccount = 'Have an account?';
+  static const login = 'Log in';
+  static const welcomeBack = 'Welcome Back👋';
+  static const letsLoginApplyToJobs = 'Let’s log in. Apply to jobs!';
+  static const forgotPassword = 'Forgot Password?';
+  static const haveNotAnAccount = 'Haven\'t an account?';
+  static const enterYourEmailOrPhoneNumberWeWillSendYouVerificationCode =
+      'Enter your email or phone number, we will send you verification code';
+  static const sendCode = 'Send Code';
+  static const verifyCode = 'Verify Code';
+  static const enterYourVerificationCodeFromYourEmailOrPhoneNumberThatWeHaveSent =
+      'Enter your verification code from your email or phone number that we’ve sent';
+  static const mustNotBeEmpty = 'must not be empty';
+  static const verify = 'Verify';
+  static const resetPassword = 'Reset Password';
+  static const enterYourNewPasswordAndConfirmTheNewPasswordToResetPassword = 'Enter your new password and confirm the new password to reset password';
+  static const confirmation = 'Confirmation';
+  static const yourPasswordHasBeenChangedPleaseLoginWithYourNewPassword = 'Your password has been changed. Please log in with your new password.';
+  static const whatTypeOfJobYouAreLookingFor = 'What type of job you’re looking for?';
+  static const designer = 'Designer';
+  static const developer = 'Developer';
+  static const administrative   = 'Administrative  ';
+  static const marketing   = 'Marketing  ';
+  static const management   = 'Management  ';
+  static const others   = 'Others  ';
+  static const fullTime   = 'Full-Time  ';
+  static const partTime   = 'Part-Time  ';
+  static const contract   = 'Contract  ';
+  static const internship   = 'Internship  ';
+  static const freelance   = 'Freelance  ';
+  static const save   = 'Save  ';
+}
